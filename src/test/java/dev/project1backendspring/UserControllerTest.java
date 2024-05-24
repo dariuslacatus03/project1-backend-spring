c4b5544325934f74a4e5c455c5e0aaf152eb19f8
dariuslacatus03 <dariusandrey2003@gmail.com>
2024-05-24 03:09:55 +0300

    secret

--- src/test/java/dev/project1backendspring/UserControllerTest.java
+++ src/test/java/dev/project1backendspring/UserControllerTest.java
@@ -1,102 +1,102 @@
-package dev.project1backendspring;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import dev.project1backendspring.controller.AnimeController;
-import dev.project1backendspring.controller.UserController;
-import dev.project1backendspring.model.Anime;
-import dev.project1backendspring.model.User;
-import dev.project1backendspring.service.UserService;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.util.List;
-
-import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(UserController.class)
-@AutoConfigureMockMvc(addFilters = false)
-public class UserControllerTest {
-    @MockBean
-    UserService userService;
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    public void testUserEndpoint_GET_Works() throws Exception {
-        User newUser = new User("user1");
-        when(userService.getAllUsers()).thenReturn(List.of(newUser, newUser));
-
-        when(userService.getUserByID(any(Long.class))).thenReturn(newUser);
-
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/shows/all-users"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$").isArray())
-                .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0].userName").value("user1"));
-
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/shows/user/1"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.userName").value("user1"));
-    }
-
-    @Test
-    public void testUserEndpoint_DELETE_Works() throws Exception {
-
-        this.mockMvc.perform(MockMvcRequestBuilders.delete("/shows/remove-user/5")
-                        .contentType(MediaType.APPLICATION_JSON)) // Set content type to JSON or any appropriate type
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8"))
-                .andExpect(content().string("User deleted"));
-        verify(userService).deleteUser(any(Long.class));
-    }
-
-    @Test
-    public void testUserEndpoint_POST_Works() throws Exception {
-        User newUser = new User("user1");
-        ObjectMapper objectMapper = new ObjectMapper();
-        String requestBody = objectMapper.writeValueAsString(newUser);
-
-        this.mockMvc.perform(MockMvcRequestBuilders.post("/shows/adduser")
-                        .contentType(MediaType.APPLICATION_JSON) // Set content type to JSON
-                        .content(requestBody)) // Set request body
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_PLAIN_VALUE+";charset=UTF-8"))
-                .andExpect(content().string("User added"));
-    }
-
-    @Test
-    public void testAnimeEndpoint_PUT_Works() throws Exception {
-
-        User updatedUser = new User("user1");
-
-        // Convert the updated tank object to JSON
-        ObjectMapper objectMapper = new ObjectMapper();
-        String requestBody = objectMapper.writeValueAsString(updatedUser);
-        this.mockMvc.perform(MockMvcRequestBuilders.put("/shows/update-user/1")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8"))
-                .andExpect(content().string("User updated"));
-        verify(userService).updateUser(any(Long.class), any(User.class));
-
-    }
-}
+//package dev.project1backendspring;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import dev.project1backendspring.controller.AnimeController;
+//import dev.project1backendspring.controller.UserController;
+//import dev.project1backendspring.model.Anime;
+//import dev.project1backendspring.model.User;
+//import dev.project1backendspring.service.UserService;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+//
+//import java.util.List;
+//
+//import static org.hamcrest.Matchers.hasSize;
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.Mockito.verify;
+//import static org.mockito.Mockito.when;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+//
+//@ExtendWith(SpringExtension.class)
+//@WebMvcTest(UserController.class)
+//@AutoConfigureMockMvc(addFilters = false)
+//public class UserControllerTest {
+//    @MockBean
+//    UserService userService;
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Test
+//    public void testUserEndpoint_GET_Works() throws Exception {
+//        User newUser = new User("user1");
+//        when(userService.getAllUsers()).thenReturn(List.of(newUser, newUser));
+//
+//        when(userService.getUserByID(any(Long.class))).thenReturn(newUser);
+//
+//        this.mockMvc.perform(MockMvcRequestBuilders.get("/shows/all-users"))
+//                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(jsonPath("$").isArray())
+//                .andExpect(jsonPath("$", hasSize(2)))
+//                .andExpect(jsonPath("$[0].userName").value("user1"));
+//
+//        this.mockMvc.perform(MockMvcRequestBuilders.get("/shows/user/1"))
+//                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(jsonPath("$.userName").value("user1"));
+//    }
+//
+//    @Test
+//    public void testUserEndpoint_DELETE_Works() throws Exception {
+//
+//        this.mockMvc.perform(MockMvcRequestBuilders.delete("/shows/remove-user/5")
+//                        .contentType(MediaType.APPLICATION_JSON)) // Set content type to JSON or any appropriate type
+//                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andExpect(content().contentType(MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8"))
+//                .andExpect(content().string("User deleted"));
+//        verify(userService).deleteUser(any(Long.class));
+//    }
+//
+//    @Test
+//    public void testUserEndpoint_POST_Works() throws Exception {
+//        User newUser = new User("user1");
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        String requestBody = objectMapper.writeValueAsString(newUser);
+//
+//        this.mockMvc.perform(MockMvcRequestBuilders.post("/shows/adduser")
+//                        .contentType(MediaType.APPLICATION_JSON) // Set content type to JSON
+//                        .content(requestBody)) // Set request body
+//                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andExpect(content().contentType(MediaType.TEXT_PLAIN_VALUE+";charset=UTF-8"))
+//                .andExpect(content().string("User added"));
+//    }
+//
+//    @Test
+//    public void testAnimeEndpoint_PUT_Works() throws Exception {
+//
+//        User updatedUser = new User("user1");
+//
+//        // Convert the updated tank object to JSON
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        String requestBody = objectMapper.writeValueAsString(updatedUser);
+//        this.mockMvc.perform(MockMvcRequestBuilders.put("/shows/update-user/1")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(requestBody))
+//                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andExpect(content().contentType(MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8"))
+//                .andExpect(content().string("User updated"));
+//        verify(userService).updateUser(any(Long.class), any(User.class));
+//
+//    }
+//}
--- src/test/java/dev/project1backendspring/UserServiceTest.java
+++ src/test/java/dev/project1backendspring/UserServiceTest.java
@@ -1,112 +1,112 @@
-package dev.project1backendspring;
-
-import dev.project1backendspring.model.Anime;
-import dev.project1backendspring.model.RepositoryException;
-import dev.project1backendspring.model.User;
-import dev.project1backendspring.repository.AnimeRepository;
-import dev.project1backendspring.repository.UserRepository;
-import dev.project1backendspring.service.AnimeService;
-import dev.project1backendspring.service.UserService;
-import jakarta.persistence.TypedQuery;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.List;
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import jakarta.persistence.EntityManager;
-
-@ExtendWith(MockitoExtension.class)
-public class UserServiceTest {
-    @Mock
-    private UserRepository userRepository;
-
-    @InjectMocks
-    private UserService userService;
-
-    @Test
-    public void testAddUser() throws RepositoryException
-    {
-        User newUser = new User("user1");
-        when(userRepository.save(any(User.class))).thenReturn(newUser);
-        userService.addUser(newUser);
-        verify(userRepository, times(1)).save(newUser);
-    }
-
-    @Test
-    public void testRemoveUser() throws RepositoryException
-    {
-        User newUser = new User("user1");
-        userService.deleteUser(newUser.getId());
-        verify(userRepository).deleteById(newUser.getId());
-    }
-
-    @Test
-    public void testGetUserById_Found() throws RepositoryException {
-        Long userId = 1L;
-        User expectedUser = new User(); // Assume Anime is a valid entity class
-        when(userRepository.findById(userId)).thenReturn(Optional.of(expectedUser));
-
-        User resultUser = userService.getUserByID(userId);
-
-        assertThat(resultUser).isNotNull();
-        assertThat(resultUser).isSameAs(expectedUser);
-    }
-
-    @Test
-    public void testGetUserById_Not_Found() throws RepositoryException {
-        Long userId = 1L;
-        // Assume Anime is a valid entity class
-        when(userRepository.findById(userId)).thenReturn(Optional.empty());
-        assertThatThrownBy(()->userService.getUserByID(userId)).isInstanceOf(RepositoryException.class).hasMessage("User with given ID not found");
-    }
-
-    @Test
-    public void deleteUser_Found() throws RepositoryException {
-        Long userId = 1L;
-
-        userService.deleteUser(userId);
-        verify(userRepository).deleteById(userId);
-    }
-
-    @Test void deleteUser_Not_Found(){
-        Long userId = 1L;
-
-        doThrow(new RuntimeException()).when(userRepository).deleteById(userId);
-        assertThatThrownBy(()->userService.deleteUser(userId)).isInstanceOf(RepositoryException.class).hasMessage("Couldn't delete user");
-
-    }
-
-    @Test void updateUser_Found() throws RepositoryException {
-        User newUser = new User("user1");
-        when(userRepository.findById(newUser.getId())).thenReturn(Optional.of(newUser));
-
-        when(userRepository.save(newUser)).thenReturn(newUser);
-
-        userService.updateUser(newUser.getId(), newUser);
-    }
-
-
-    @Test void getAll_Working() throws RepositoryException {
-
-        User newUser = new User("user1");
-        List<User> list = List.of(newUser, newUser);
-
-        when(userRepository.findAll()).thenReturn(list);
-
-        assertThat(userService.getAllUsers().get(0).getId()).isSameAs(list.get(0).getId());
-        assertThat(userService.getAllUsers().get(1).getId()).isSameAs(list.get(1).getId());
-    }
-
-    @Test void getAll_Not_Working() throws RepositoryException {
-
-        doThrow(new RuntimeException()).when(userRepository).findAll();
-        assertThatThrownBy(()->userService.getAllUsers()).isInstanceOf(RepositoryException.class);
-    }
-}
+//package dev.project1backendspring;
+//
+//import dev.project1backendspring.model.Anime;
+//import dev.project1backendspring.model.RepositoryException;
+//import dev.project1backendspring.model.User;
+//import dev.project1backendspring.repository.AnimeRepository;
+//import dev.project1backendspring.repository.UserRepository;
+//import dev.project1backendspring.service.AnimeService;
+//import dev.project1backendspring.service.UserService;
+//import jakarta.persistence.TypedQuery;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//import static org.assertj.core.api.Assertions.*;
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.Mockito.*;
+//import jakarta.persistence.EntityManager;
+//
+//@ExtendWith(MockitoExtension.class)
+//public class UserServiceTest {
+//    @Mock
+//    private UserRepository userRepository;
+//
+//    @InjectMocks
+//    private UserService userService;
+//
+//    @Test
+//    public void testAddUser() throws RepositoryException
+//    {
+//        User newUser = new User("user1");
+//        when(userRepository.save(any(User.class))).thenReturn(newUser);
+//        userService.addUser(newUser);
+//        verify(userRepository, times(1)).save(newUser);
+//    }
+//
+//    @Test
+//    public void testRemoveUser() throws RepositoryException
+//    {
+//        User newUser = new User("user1");
+//        userService.deleteUser(newUser.getId());
+//        verify(userRepository).deleteById(newUser.getId());
+//    }
+//
+//    @Test
+//    public void testGetUserById_Found() throws RepositoryException {
+//        Long userId = 1L;
+//        User expectedUser = new User(); // Assume Anime is a valid entity class
+//        when(userRepository.findById(userId)).thenReturn(Optional.of(expectedUser));
+//
+//        User resultUser = userService.getUserByID(userId);
+//
+//        assertThat(resultUser).isNotNull();
+//        assertThat(resultUser).isSameAs(expectedUser);
+//    }
+//
+//    @Test
+//    public void testGetUserById_Not_Found() throws RepositoryException {
+//        Long userId = 1L;
+//        // Assume Anime is a valid entity class
+//        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+//        assertThatThrownBy(()->userService.getUserByID(userId)).isInstanceOf(RepositoryException.class).hasMessage("User with given ID not found");
+//    }
+//
+//    @Test
+//    public void deleteUser_Found() throws RepositoryException {
+//        Long userId = 1L;
+//
+//        userService.deleteUser(userId);
+//        verify(userRepository).deleteById(userId);
+//    }
+//
+//    @Test void deleteUser_Not_Found(){
+//        Long userId = 1L;
+//
+//        doThrow(new RuntimeException()).when(userRepository).deleteById(userId);
+//        assertThatThrownBy(()->userService.deleteUser(userId)).isInstanceOf(RepositoryException.class).hasMessage("Couldn't delete user");
+//
+//    }
+//
+//    @Test void updateUser_Found() throws RepositoryException {
+//        User newUser = new User("user1");
+//        when(userRepository.findById(newUser.getId())).thenReturn(Optional.of(newUser));
+//
+//        when(userRepository.save(newUser)).thenReturn(newUser);
+//
+//        userService.updateUser(newUser.getId(), newUser);
+//    }
+//
+//
+//    @Test void getAll_Working() throws RepositoryException {
+//
+//        User newUser = new User("user1");
+//        List<User> list = List.of(newUser, newUser);
+//
+//        when(userRepository.findAll()).thenReturn(list);
+//
+//        assertThat(userService.getAllUsers().get(0).getId()).isSameAs(list.get(0).getId());
+//        assertThat(userService.getAllUsers().get(1).getId()).isSameAs(list.get(1).getId());
+//    }
+//
+//    @Test void getAll_Not_Working() throws RepositoryException {
+//
+//        doThrow(new RuntimeException()).when(userRepository).findAll();
+//        assertThatThrownBy(()->userService.getAllUsers()).isInstanceOf(RepositoryException.class);
+//    }
+//}
